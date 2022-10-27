46752f69d97499a9867c5d60ce40aad55dd247e7
SimplifyOrg <abhishek.en@gmail.com>
2022-10-28 00:31:08 +0530

    Add files via upload

---
+++ UI/src/components/sidebar/Sidepanel.js
@@ -0,0 +1,70 @@
+import React, {useState, useContext} from 'react'
+import {
+    Box,
+    IconButton,
+    Flex,
+    Heading,
+    Text,
+    Avatar,
+    VStack
+} from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
+import UserContext from '../../utils/UserContext'
+
+function Sidepanel() {
+
+    const {user} = useContext(UserContext)
+  return (
+    <Flex
+        w="100%"
+        flexDir="column"
+        alignItems="center"
+        backgroundColor="orange.100"
+        color="orange.400"
+        maxW="130px"
+    >
+        <Flex
+            flexDir="column"
+            justifyContent="space-between"
+            height="90%"
+        >
+            <Flex flexDir="column" as="nav">
+                <Flex
+                    flexDir="column"
+                    align="flex-start"
+                    justifyContent="center"
+                >
+                    <Flex className='sidebar-items'>
+                        <Link _hover={{textDecor:'none'}} to='/dashboard'>
+                            <Text className="active">Dashboard</Text>
+                        </Link>
+                    </Flex>
+                    <Flex className='sidebar-items'>
+                        <Link _hover={{textDecor:'none'}} to='/farms'>
+                            <Text>Farms</Text>
+                        </Link>
+                    </Flex>
+                    <Flex className='sidebar-items'>
+                        <Link _hover={{textDecor:'none'}}>
+                            <Text>Workflows</Text>
+                        </Link>
+                    </Flex>
+                    <Flex className='sidebar-items'>
+                        <Link _hover={{textDecor:'none'}}>
+                            <Text>Calender</Text>
+                        </Link>
+                    </Flex>
+                </Flex>
+            </Flex>
+            <Flex flexDir="column" alignItems="center" mb={10} mt={5}>
+                <Avatar my={2} name={user.data.first_name +' '+ user.data.last_name}  src={user.picture.image}/>
+                <Text>{user.data.first_name}</Text>
+                <Text>{user.data.last_name}</Text>
+            </Flex>
+        </Flex>
+
+    </Flex>
+  )
+}
+
+export default Sidepanel
