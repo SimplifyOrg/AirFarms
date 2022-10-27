95739bf221a95cd32b2f27a9927f69c35d51ed28
SimplifyOrg <abhishek.en@gmail.com>
2022-10-28 00:35:04 +0530

    Add files via upload

---
+++ UI/src/tools/Notifications.js
@@ -0,0 +1,88 @@
+import React, {useState, useEffect, useContext} from 'react'
+import { BellIcon } from '@chakra-ui/icons'
+import {IconButton} from '@chakra-ui/button'
+import UserContext from '../utils/UserContext'
+import {
+    Menu,
+    MenuButton,
+    MenuList,
+    MenuItem,
+    Button
+  } from '@chakra-ui/react'
+
+import NotificationBadge from '../components/notifications/NotificationBadge'
+import NotificationItem from '../components/notifications/NotificationItem'
+
+function Notifications() {
+
+    const { user } = useContext(UserContext)
+    let initialNotifications = []
+    let initNotifications = new Set()
+    const [notifications, SetNotifications] = useState(initialNotifications)
+    const [websocket, SetWebsocket] = useState(null)
+
+    useEffect(() => {
+        //TODO: make changes to receive notifications
+        if(user.data.id !== undefined && websocket === null)
+        {
+            //const websocketLink = `ws://airfa-loadb-k74y8ct6ljs9-46c5c512b06feb5a.elb.ap-south-1.amazonaws.com:8000/messaging/notification/${user.data.id}`
+            const websocketLink = `ws://127.0.0.1:8000/messaging/notification/${user.data.id}`
+            const webSock = new WebSocket(websocketLink)
+            SetWebsocket(webSock)
+
+
+            webSock.onopen = () => {
+                // on connecting, do nothing but log it to the console
+                console.log('connected')
+            }
+
+            webSock.onmessage = evt => {
+                // listen to data sent from the websocket server
+                const message = JSON.parse(evt.data)
+                let messages = JSON.parse(message.data.value)
+                for(let i = 0; i < messages.length; ++i)
+                {
+                    if(!initNotifications.has(messages[i].id))
+                    {
+                        initNotifications.add(messages[i].id)
+                        initialNotifications.push(messages[i])
+                    }                    
+                }
+                SetNotifications(initialNotifications.slice())
+                console.log(message)
+            }
+
+            webSock.onerror = function(error) {
+                alert(`[error] ${error.message}`);
+            }
+
+        }
+
+    }, []);
+
+    useEffect(() => {
+        //TODO: make changes to receive notifications        
+
+    }, [notifications]);
+
+    return (
+        <Menu>
+            <MenuButton ml={8}>
+                { <NotificationBadge count={notifications.length}/>}
+            </MenuButton>
+            <MenuList>
+            {
+                notifications.length === 0 ? <MenuItem>All catched up!</MenuItem>: Array.from(notifications).map((notificationBody, idx) => {
+                return(
+                    <MenuItem>
+                        <NotificationItem key={idx} notificationBody={notificationBody}/>
+                    </MenuItem>
+                )
+                })
+            }
+            </MenuList>
+        </Menu>
+    )
+}
+
+export default Notifications
--- UI/src/tools/Workflow.js
+++ UI/src/tools/Workflow.js
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect} from 'react'
+import React, {useContext, useState, useEffect, useCallback} from 'react'
 import WorkflowDiagram from './activity/WorkflowDiagram'
 // import NavBar from '../components/NavBar'
 import {Box} from '@chakra-ui/react'
@@ -6,23 +6,27 @@ import { useLocation } from "react-router-dom"
 import FarmContext from '../utils/FarmContext'
 import WorkflowContext from '../utils/WorkflowContext'
 import UserContext from '../utils/UserContext'
+import JsonFlowContext from '../utils/JsonFlowContext'
+import NavBar from '../components/navigation/NavBar'
+import { AuthProvider } from '../utils/AuthProvider'
 
 function Workflow() {
-    const {workflow, setWorkflow} = useContext(WorkflowContext)
+    const location = useLocation();
+    const [currworkflow, SetCurrworkflow] = useState(location?.state?.workflow)
+    const {workflow} = useContext(WorkflowContext)
     const {farm} = useContext(FarmContext)
     const {user} = useContext(UserContext)
+    const {jsonFlow, setJsonFlow} = useContext(JsonFlowContext)
 
-    useEffect(() => {   
-        const workflowInit = {
-            workflow: {
-                id: "1",
-                title: "New workflow",
-                currentStates: [1],
-                has_finished: "false",
-                farm: farm,
-                owner: user.id,
-                is_production: "false"
-            },
+    useEffect(() => {
+        
+        let inComingWorkflow = undefined
+        if(location?.state?.workflow !== null && location?.state?.workflow !== undefined)
+        {
+            inComingWorkflow = location?.state?.workflow;
+        }
+        
+        let init = {
             nodes: [
                 {
                     id: 'n1',
@@ -36,51 +40,157 @@ function Workflow() {
                     },
                     position: { x: 50, y: 5 },
                 },
-                {
-                    id: 'n2',
-                    type: 'workflowNode',
-                    data: { 
-                        label: 'end',
-                        title: 'end',
-                        notifiers: [user.id],
-                        notes: "",
-                        works: []
-                    },
-                    position: { x: 250, y: 5 },
-                },
             ],
-            edges: [
+            edges: []
+        }
+
+        if(currworkflow === undefined && inComingWorkflow === undefined)
+        {
+            // Most probably this is dead code
+            // keeping this to handle exception scenarios.            
+            let workflowInit = {
+                workflow: {
+                    id: "-1",
+                    title: "New workflow",
+                    currentStates: ['n1'],
+                    has_finished: "false",
+                    farm: farm,
+                    owner: user.id,
+                    is_production: "false"
+                }
+
+            }
+            const workflowJSON = {...workflowInit, ...init} 
+            const flowJSON = JSON.stringify(workflowJSON);
+            SetCurrworkflow(flowJSON)
+            localStorage.setItem(workflow, flowJSON);
+        }
+        else
+        {
+            // When new workflow is being created add default nodes and edges
+            let initialWorkflow = inComingWorkflow === undefined? currworkflow : inComingWorkflow
+            if(initialWorkflow.nodes === undefined || initialWorkflow.edges === undefined)
+            {
+                let present = false;
+                for (let i = 0; i < initialWorkflow.workflow.currentStates.length; ++i)
                 {
-                    id: '1',
-                    source: 'n1',
-                    target: 'n2',
-                    type: 'buttonedge',
-                    sourceHandle: "a",
-                    animated: true,
-                    data: { 
-                        label:'Approval', 
-                        text: 'New transition',
-                        transition: 
-                        {
-                            id: "1",
-                            previous: "n1",
-                            next: "n2",
-                            associatedFlow: "1",
-                            need_approval: false,
-                            transitionapprovals: []
-                        }
+                    if(initialWorkflow.workflow.currentStates[i] === init.nodes[0].id)
+                    {
+                        present = true;
                     }
                 }
-            ]
+                if(!present)
+                {
+                    initialWorkflow.workflow.currentStates.push(init.nodes[0].id)
+                }   
+                
+                const workflowJSON = {...initialWorkflow, ...init}
+                // const flowJSON = JSON.stringify(workflowJSON);
+                SetCurrworkflow(workflowJSON)
+                localStorage.setItem(workflow, JSON.stringify(workflowJSON));
+            }
+            else
+            {
+                // SetCurrworkflow(JSON.stringify(initialWorkflow))
+                localStorage.setItem(workflow, JSON.stringify(initialWorkflow));
+            }
+            // localStorage.setItem(workflow, JSON.stringify(initialWorkflow));
+        }
 
-        }     
-        const flowJSON = JSON.stringify(workflowInit);
-        setWorkflow(flowJSON)
     }, [])
 
+    const createWorkflow = (authProvider, dataWorkflow, workflowObj, config) => {
+        authProvider.authPost(`/activity/workflow/handle/`, dataWorkflow, config, false)
+        .then(resWorkflow =>{
+            console.log(resWorkflow)
+            if(workflow !== null)
+            {
+                // Update workflow id with the database id of the workflow
+                workflowObj.workflow.id = resWorkflow.data.id
+                const data = {
+                    jsonFlow: JSON.stringify(workflowObj),
+                    workflow: resWorkflow.data.id,
+                    farm: farm.id
+                }
+                authProvider.authPost(`/activity/json-workflow/handle/`, data, config, false)
+                .then(res =>{
+                    console.log(res);
+                    console.log(res.data);
+                    // set workflow context with updated workflow object with database ids
+                    SetCurrworkflow(res.data.jsonFlow)
+                    setJsonFlow(res.data)
+                    localStorage.setItem(workflow, res.data.jsonFlow);
+                    // SetReactFlowInstance(JSON.parse(res.data.jsonFlow))
+                })
+                .catch(error => {
+                    console.log(error);
+                    console.log(error.data);
+                })  
+            }
+        })
+        .catch(errorWorkflow => {
+            console.log(errorWorkflow);
+            console.log(errorWorkflow.data);
+        })
+    }
+
+    // const updateWorkflow = useCallback( (workflowObj) => {
+
+    // }, [])
+
+    const patchWorkflow = useCallback( (workflowObj) => {
+        const authProvider = AuthProvider()
+        const JSONdata = {
+            jsonFlow: JSON.stringify(workflowObj),
+            workflow: workflowObj.workflow.id,
+            farm: farm.id
+        }
+        let config = {
+            headers: {
+                'Accept': 'application/json'
+            }
+        }
+        authProvider.authGet(`/activity/json-workflow/handle/?workflow=${workflowObj.workflow.id}`, config)
+        .then(res =>{
+            console.log(res);
+            console.log(res.data);
+            if(res.data.length !== 0)
+            {
+                authProvider.authPut(`/activity/json-workflow/handle/${res.data[0].id}/`, JSONdata, config)
+                .then(resJSON =>{
+                    console.log(resJSON);
+                    console.log(resJSON.data);
+                    // set workflow context with updated workflow object with database ids
+                    SetCurrworkflow(resJSON.data.jsonFlow)
+                    setJsonFlow(resJSON.data)
+                    localStorage.setItem(workflow, resJSON.data.jsonFlow);
+                    // setReactFlowInstance(JSON.parse(resJSON.data.jsonFlow))
+                })
+                .catch(errorJSON => {
+                    console.log(errorJSON);
+                    console.log(errorJSON.data);
+                })
+            }
+            else
+            {
+                const dataWorkflow = {
+                    farm: farm.id,
+                    owner: user.data.id,
+                    title: workflowObj.workflow.title
+                }
+                createWorkflow(authProvider, dataWorkflow, workflowObj, config)
+            }
+            // set workflow context with updated workflow object with database ids
+            // SetCurrworkflow(resJSON.data.jsonFlow)
+        })
+        .catch(error => {
+            console.log(error);
+            console.log(error.data);
+        }) 
+    })
+
   return (
-        <>
-            {/* <NavBar/> */}
+        <NavBar>
             <Box 
             display='flex' 
             p='3' 
@@ -92,9 +202,9 @@ function Workflow() {
             ml='2'
             mr='2'
             > 
-                <WorkflowDiagram/>
+                <WorkflowDiagram patchWorkflow={patchWorkflow} workflow={currworkflow}/>
             </Box>
-        </>
+        </NavBar>
   )
 }
 
