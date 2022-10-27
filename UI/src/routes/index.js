b430647b1874d0bbb553a751226cf9356e571882
SimplifyOrg <abhishek.en@gmail.com>
2022-10-28 00:33:15 +0530

    Add files via upload

--- UI/src/routes/index.js
+++ UI/src/routes/index.js
@@ -16,6 +16,8 @@ import Logout from '../pages/Logout'
 // import TodoPage from '../pages/TodoPage'
 import Workflow from '../tools/Workflow';
 import FarmList from '../components/farm/FarmList';
+import Dashboard from '../pages/Dashboard';
+import WorkflowList from '../components/workflow/WorkflowList';
 
 function Navigation() {
     return (
@@ -26,6 +28,7 @@ function Navigation() {
             <Route path="login" exact element={<Signin/>} />
             <Route path="logout" exact isPrivate element={<Logout/>} />
             <Route path="farms" exact isPrivate element={<FarmList/>} />
+            <Route path="dashboard" exact isPrivate element={<Dashboard/>} />
             {/* <RouteWrapper path="/resetpassword" exact component={ResetPassword} />
             <RouteWrapper path="/user-dashboard" exact isPrivate component={UserDashboard} />
             <RouteWrapper path="/create-farm" exact isPrivate component={CreateFarm} />
@@ -35,6 +38,7 @@ function Navigation() {
             <RouteWrapper path="/discussion-board" exact isPrivate component={DiscussionBoardPage} />
             <RouteWrapper path="/todo" exact isPrivate component={TodoPage} /> */}
             <Route path="workflow" exact isPrivate element={<Workflow/>} />
+            <Route path="workflow-list" exact isPrivate element={<WorkflowList/>} />
             <Route element={<Signin/>}/>
         </Routes>
         
