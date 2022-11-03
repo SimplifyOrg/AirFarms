15cac351c9c59cb740d32921c8693f9faacb7eb0
SimplifyOrg <abhishek.en@gmail.com>
2022-11-03 22:44:27 +0530

    Add files via upload

--- UI/src/components/notifications/NotificationItem.js
+++ UI/src/components/notifications/NotificationItem.js
@@ -2,12 +2,18 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 
 function NotificationItem(props) {
+    const messageMap = {
+        1:`You are added as an approver to a transistion`,
+        2:`A new work is assigned to you`,
+        3:`Some tasks are overdue`
+
+    }
   return (
     <Link to={{
         pathname: "/load-task",
         state: { task: props.notificationBody },
       }}>
-          {props.notificationBody.notification_type}
+          {messageMap[props.notificationBody.notification_type]}
     </Link>
   )
 }
