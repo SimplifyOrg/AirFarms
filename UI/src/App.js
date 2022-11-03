4f300cead4f236b73768f87730018498f2b002d1
SimplifyOrg <abhishek.en@gmail.com>
2022-11-03 22:50:55 +0530

    Add files via upload

--- UI/src/App.js
+++ UI/src/App.js
@@ -2,34 +2,30 @@ import logo from './logo.svg';
 import React, {useState} from 'react'
 import './App.css';
 // import Login from './pages/Login';
-import UserContext from './utils/UserContext'
-import FarmContext from './utils/FarmContext';
-import WorkflowContext from './utils/WorkflowContext';
+// import UserContext from './utils/UserContext'
+import UserProvider from './utils/UserProvider';
+import FarmProvider from './utils/FarmProvider';
+import WorkflowProvider from './utils/WorkflowProvider';
 import {BrowserRouter} from 'react-router-dom'
 import Navigation from './routes/index'
 import NodeContext from './utils/NodeContext';
 import JsonFlowContext from './utils/JsonFlowContext';
 
 function App() {
-    const [user, setUser] = useState({});
-    const valueUser = { user, setUser };
-
-    const [farm, setFarm] = useState({});
-    const valueFarm = { farm, setFarm };
-
-    const [workflow, setWorkflow] = useState({});
-    const valueWorkflow = { workflow, setWorkflow };
 
     const [node, setNode] = useState({});
     const valueNode = { node, setNode };
 
     const [jsonFlow, setJsonFlow] = useState({});
     const valueJson = { jsonFlow, setJsonFlow };
+
+    
+
   return (
     <div className="App">
-        <UserContext.Provider value={valueUser}>
-            <FarmContext.Provider value={valueFarm}>
-                <WorkflowContext.Provider value={valueWorkflow}>
+        <UserProvider>
+            <FarmProvider>
+                <WorkflowProvider>
                     <JsonFlowContext.Provider value={valueJson}>
                         <NodeContext.Provider value={valueNode}>
                             <BrowserRouter>
@@ -37,9 +33,9 @@ function App() {
                             </BrowserRouter>
                         </NodeContext.Provider>
                     </JsonFlowContext.Provider>
-                </WorkflowContext.Provider>
-            </FarmContext.Provider>
-        </UserContext.Provider>      
+                </WorkflowProvider>
+            </FarmProvider>
+        </UserProvider>      
     </div>
   );
 }
