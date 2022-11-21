70e2669bb0388535352bfef0c5f8168b766113df
SimplifyOrg <abhishek.en@gmail.com>
2022-11-21 09:06:13 +0530

    Add files via upload

--- Backend/scripts/run.sh
+++ Backend/scripts/run.sh
@@ -2,8 +2,9 @@
 
 set -e
 
-python3 manage.py collectstatic --noinput
-python3 manage.py migrate
+# python3 manage.py collectstatic --noinput
+# python3 manage.py makemigrations
+# python3 manage.py migrate
 #service start /etc/systemd/system/airfarms-daphne.service
 
 uwsgi --socket :9000 --workers 4 --master --enable-threads -b 65535 --module machhli.wsgi & daphne -b 0.0.0.0 -p 9001 machhli.asgi:application
