d7f4e37066799d40f5b8c4edff169809fcce3db1
SimplifyOrg <abhishek.en@gmail.com>
2022-12-07 02:33:54 +0530

    Add files via upload

--- Backend/machhli/settings.py
+++ Backend/machhli/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*1ukfouf7nmoof!wucqw3l9%ra_yn*8fv!iluq!_c56cy0a8b#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
