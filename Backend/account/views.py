d44accf70eac7def6845c92f7c5ac669a6535b3f
SimplifyOrg <abhishek.en@gmail.com>
2022-12-07 02:30:34 +0530

    Add files via upload

--- Backend/account/views.py
+++ Backend/account/views.py
@@ -8,15 +8,16 @@ from rest_framework.views import APIView
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework.response import Response
 from rest_framework import status
-from django.contrib.auth.models import Group
+from rest_framework.filters import SearchFilter, OrderingFilter
 # Create your views here.
 
 class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
     queryset = User.objects.all()
-    filter_backends = [DjangoFilterBackend,]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter,]
     filterset_fields = ['id', 'groups']
     ordering_fields = ['first_name', 'last_name', 'email']
+    search_fields = ['first_name', 'email', 'phonenumber', 'last_name']
 
 class ProfilePictureViewSet(viewsets.ModelViewSet):
     serializer_class = ProfilePictureSerializer
